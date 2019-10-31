00fe7b82b68176cd12447e0285747bf8e943d2fd
Colorwayfiend <51940653+Colorwayfiend@users.noreply.github.com>
2019-10-30 23:08:11 -0500

    final commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,7 +2,7 @@
 var ufos = data
 var tbody = d3.select("tbody");
 
-// Loop Through `data` and console.log each report object
+// Loop Through `data` 
 ufos.forEach(function (sightings) {
 
 });
@@ -19,35 +19,26 @@ ufos.forEach(function (sightings) {
     });
 });
 
-sightings(ufos)
-var button = d3.select("#filter-btn”)
-var date = d3.select("#datetime”);
-function input() {
-        d3.event.preventDefault();
-        console.log(date.property("value”));
-        var filteredData = ufos.filter(sighting => sighting.datetime === dateEntry.property("value”))
-        sightings(filteredData);
-    }
-date.on("change”, input);
-// Get a reference to the table body
-var tbody = d3.select(“tbody”);
-//Console.log the weather data from data.js
-console.log(data);
+//Select button instructions
+var button = d3.select("#filter-btn");
+button.on("click", function () {
+    tbody.html("");
+    var filteredData = ufos
+    //Select the input element and get the raw HTML node
+    var dates = d3.select("#datetime");
+    //Get the value property of the input element
+    var i_element = dates.property("value");
+    filteredData = filteredData.filter(ufos => ufos.datetime === i_element)
 
-
-
-// sightings(ufos)
-// var button = d3.select("#filter-btn");
-// var date = d3.select("#datetime");
-// function date() {
-//     d3.event.preventDefault();
-//     console.log(input.property("value"));
-//     var filteredData = ufos.filter(sighting => sighting.datetime === dateEntry.property("value"));
-//     sightings(filteredData);
-// };
-// date.on("change", date);
-// // Get a reference to the table body
-// var tbody = d3.select(“tbody”);
-// //Console.log the data from data.js
-// console.log(ufos);
-
+    tbody = d3.select("tbody");
+    filteredData.forEach(function (sightings) {
+        var row = tbody.append("tr");
+        Object.entries(sightings).forEach(function ([key, value]) {
+            console.log(key, value);
+            // Append a cell to the row for each value
+            // in the report object
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+});
